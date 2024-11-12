5b04099ba64413657bb303e5530c43c397db6dde
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-11-12 17:44:41 +0400

    feat: add RabbitMQ integration with RmqModule and RmqService for billing module

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,5 @@
 export * from './database/database.module';
 export * from './database/abstract.repsitory';
 export * from './database/abstract.schema';
+export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
