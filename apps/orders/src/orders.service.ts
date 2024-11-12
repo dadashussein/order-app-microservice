d150b5f4fa394a5b7b6ee47b647dcced2dc93afc
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-11-12 16:55:22 +0400

    feat: implement order creation and validation with DTOs

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order-request';
+import { OrderRepository } from './order.repsitory';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private readonly ordersRepository: OrderRepository) { }
+
+  async createOrder(request: CreateOrderRequest) {
+    return this.ordersRepository.create(request);
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find({})
   }
 }
